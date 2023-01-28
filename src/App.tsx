e64888aa5347911be8c81187c73e85035924fa4e
Adam <N00211418@iadt.ie>
2023-01-28 11:40:39 +0000

    initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useRef, useState} from 'react';
+import { TodoList } from './components/TodoList';
 
 function App() {
+
+  const [todo, setTodo] = useState<string>('');
+
+  const handleInput = (event: React.SyntheticEvent): void => {
+    setTodo((event.target as HTMLInputElement).value)
+    console.log(todo)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* an input field */}
+      <input type="text" onChange={ event => handleInput(event) } />
+      {/* creates todo component */}
+      {/* inside of a todo list component */}
+      {/* <TodoList  /> */}
     </div>
   );
 }
