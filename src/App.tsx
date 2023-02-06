53f968ed340799bbe6b3af7d1b63b5ef4da8908e
Adam <N00211418@iadt.ie>
2023-02-06 20:44:13 +0000

    can add new todos / display them

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import { TodoList } from './components/TodoList';
 
 function App() {
 
-  const [todo, setTodo] = useState<string>('');
-
-  const handleInput = (event: React.SyntheticEvent): void => {
-    setTodo((event.target as HTMLInputElement).value)
-    console.log(todo)
-  }
-
 
   return (
     <div className="App">
       {/* an input field */}
-      <input type="text" onChange={ event => handleInput(event) } />
       {/* creates todo component */}
       {/* inside of a todo list component */}
-      {/* <TodoList  /> */}
+      <TodoList  />
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,38 @@
 
-import React from 'react'
+import { useState, useEffect } from 'react'
+
+interface todo {
+  name: string,
+  id: number
+}
 
 
+export const TodoList = () => {
+  const [todos, setTodos] = useState<todo[]>([])
+  const [todo, setTodo] = useState<string>('')
 
-export const TodoList = (todo: string) => {
+  useEffect(() => {
+    console.log(todos)
+  }, [todos])
+
+  const handleInput = (event: React.SyntheticEvent): void => {
+    setTodo((event.target as HTMLInputElement).value)
+  }
+
+  const createTodo = (text: string) => {
+    setTodos([...todos, { name: text, id: todos.length }])
+    setTodo('')
+  }
+
   return (
     <div>
-
+      <input type="text" value={todo} onChange={event => handleInput(event)} />
+      <button type="submit" onClick={() => createTodo(todo)}>New Todo</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
